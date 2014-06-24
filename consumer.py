18db4787032d7ecc5ec256dcb9d34f97fbd978ae
chrisalexander <chris.alexander@import.io>
2014-06-24 09:34:28 +0100

    Make things pretty

--- consumer.py
+++ consumer.py
@@ -9,7 +9,16 @@ import boto
 import time
 import config
 
-stream_name = raw_input("Please enter stream name to consume: ")
+# Vanity
+class bcolours:
+	HEADER = "\033[95m"
+	OKBLUE = "\033[94m"
+	OKGREEN = "\033[92m"
+	WARNING = "\033[93m"
+	FAIL = "\033[91m"
+	ENDC = "\033[0m"
+
+stream_name = raw_input("Please enter " + bcolours.OKBLUE + "stream name" + bcolours.ENDC + " to consume: ")
 
 kinesis = boto.connect_kinesis(aws_access_key_id=config.AWS_ACCESS_KEY_ID, aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY)
 
@@ -19,27 +28,26 @@ while tries < 10:
 	tries += 1
 	time.sleep(15)
 	response = kinesis.describe_stream(stream_name)
-	if response['StreamDescription']['StreamStatus'] == 'ACTIVE':
-		shard_id = response['StreamDescription']['Shards'][0]['ShardId']
+	if response["StreamDescription"]["StreamStatus"] == "ACTIVE":
+		shard_id = response["StreamDescription"]["Shards"][0]["ShardId"]
 		break
 else:
-	raise TimeoutError('Stream is still not active, aborting...')
+	raise TimeoutError("Stream is still not active, " + bcolours.FAIL + "aborting" + bcolours.ENDC + "...")
 
-print "Kinesis stream is active, starting consumer..."
+print "Kinesis stream is " + bcolours.OKGREEN + "active" + bcolours.ENDC + ", starting consumer..."
 
-response = kinesis.get_shard_iterator(stream_name, shard_id, 'TRIM_HORIZON')
-shard_iterator = response['ShardIterator']
+response = kinesis.get_shard_iterator(stream_name, shard_id, "TRIM_HORIZON")
+shard_iterator = response["ShardIterator"]
 
 while True:
 	time.sleep(1)
 
 	response = kinesis.get_records(shard_iterator)
-	shard_iterator = response['NextShardIterator']
+	shard_iterator = response["NextShardIterator"]
 
-	if len(response['Records']):
-		print "Found %i records" % len(response["Records"])
+	if len(response["Records"]):
 		for record in response["Records"]:
-			print record
-		print ""
+			data = json.loads(record["Data"])
+			print bcolours.OKGREEN + "Received" + bcolours.ENDC + ": " + data["title"]
 	else:
-		print "No records, continuing"
+		print bcolours.OKBLUE + "No records" + bcolours.ENDC + ", continuing"
--- server.py
+++ server.py
@@ -9,6 +9,15 @@ import boto
 import time
 import config
 
+# Vanity
+class bcolours:
+	HEADER = "\033[95m"
+	OKBLUE = "\033[94m"
+	OKGREEN = "\033[92m"
+	WARNING = "\033[93m"
+	FAIL = "\033[91m"
+	ENDC = "\033[0m"
+
 port = 8888
 if len(sys.argv) > 1:
 	port = int(sys.argv[1])
@@ -21,7 +30,7 @@ class ImportIoCrawlerHandler(BaseHTTPRequestHandler):
 	def do_POST(self):
 		global kinesis
 		global stream_name
-		length = int(self.headers['content-length'])
+		length = int(self.headers["content-length"])
 		data = self.rfile.read(length)
 		try:
 			json_data = json.loads(data)
@@ -33,18 +42,23 @@ class ImportIoCrawlerHandler(BaseHTTPRequestHandler):
 				send_data = json.dumps(index_data)
 				send_hash = md5.new(send_data).hexdigest()
 				kinesis.put_record(stream_name, send_data, send_hash)
+				print bcolours.OKGREEN + "Queued" + bcolours.ENDC + ": " + index_data["title"]
 			self.send_response(200);
 			self.end_headers()
 		except Exception as e:
-			print "Unable to process: %s; %s" % (data, e)
+			print bcolours.FAIL + "Unable to process" + bcolours.ENDC + ": %s; %s" % (data, e)
 			self.send_response(500)
 			self.end_headers()
+
+	# Silencio!
+	def log_message(self, format, *args):
+		return
 
 def main():
 	global kinesis
 	global stream_name
 
-	print "Starting Kinesis, please wait before starting the crawler!"
+	print "Starting Kinesis, " + bcolours.WARNING + "please wait before starting the crawler" + bcolours.ENDC + "!"
 	kinesis = boto.connect_kinesis(aws_access_key_id=config.AWS_ACCESS_KEY_ID, aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY)
 
 	# Create steam
@@ -56,23 +70,26 @@ def main():
 		tries += 1
 		time.sleep(15)
 		response = kinesis.describe_stream(stream_name)
-		if response['StreamDescription']['StreamStatus'] == 'ACTIVE':
-			shard_id = response['StreamDescription']['Shards'][0]['ShardId']
+		if response["StreamDescription"]["StreamStatus"] == "ACTIVE":
+			shard_id = response["StreamDescription"]["Shards"][0]["ShardId"]
 			break
 	else:
-		raise TimeoutError('Stream is still not active, aborting...')
+		raise TimeoutError("Stream is still not active, " + bcolours.FAIL + "aborting" + bcolours.ENDC + "...")
 
-	print "Kinesis stream is active, starting server..."
+	print "Kinesis stream is " + bcolours.OKGREEN + "active" + bcolours.ENDC + ", starting server..."
+
+	print "Kinesis stream name: " + bcolours.HEADER + stream_name + bcolours.ENDC
 
 	try:
-		server = HTTPServer(('', port), ImportIoCrawlerHandler)
-		print "Server started on port %i, ready to recieve crawl data" % port
+		server = HTTPServer(("", port), ImportIoCrawlerHandler)
+		print "Server started on port " + str(port) + ", " + bcolours.OKGREEN + "ready to recieve crawl data" + bcolours.ENDC
 		server.serve_forever()
 	except KeyboardInterrupt:
-		print "Server shutting down"
+		print "Server " + bcolours.FAIL + "shutting down" + bcolours.ENDC
 		server.socket.close()
-		print "Deleting Kinesis stream"
+		print bcolours.FAIL + "Deleting" + bcolours.ENDC + " Kinesis stream"
 		kinesis.delete_stream(stream_name)
+		print bcolours.OKGREEN + "Done" + bcolours.ENDC
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	main()
